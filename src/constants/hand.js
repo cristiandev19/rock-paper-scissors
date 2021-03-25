e82dc299b56d72d8a85dfd75ecf87ee19c435a33
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-24 23:30:32 -0500

    constantes para no cagarla

---
+++ src/constants/hand.js
@@ -0,0 +1,22 @@
+const handOptions = {
+  rock     : 'Rock',
+  paper    : 'Paper',
+  scissors : 'Scissors',
+};
+
+exports.handOptions = handOptions;
+
+exports.handKeyOptions = [
+  {
+    id  : handOptions.rock,
+    key : 'r',
+  },
+  {
+    id  : handOptions.paper,
+    key : 'p',
+  },
+  {
+    id  : handOptions.scissors,
+    key : 's',
+  },
+];
